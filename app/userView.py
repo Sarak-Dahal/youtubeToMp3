c380e5e763ba0a2409dcf4855deef0fae47e0f6a
Sarak Dahal <ssagamersdignity@gmail.com>
2022-08-15 14:05:10 +0545

    First

--- app/userView.py
+++ app/userView.py
@@ -4,24 +4,43 @@ from app import app
 import yt_dlp
 from yt_dlp.utils import DownloadError
 import os
+
 dirname = os.path.dirname(__file__)
-print(dirname)
 filename = os.path.join(dirname, 'downloads/')
-print("________________________")
-print(filename)
 
 
-# Routing to home root
 @app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/mp3')
+def mp3home():
+    tp = 'Mp3'
+    source = 'YouTube'
+    return render_template('index.html', tp=tp, source=source)
+
+
+@app.route('/mp4')
+def mp4home():
+    tp = 'Mp4'
+    source = 'YouTube'
+    return render_template('index.html', tp=tp, source=source)
+
+
+@app.route('/tiktok')
+def tiktokhome():
+    tp = 'Video'
+    source = 'TikTok'
+    return render_template('index.html', tp=tp, source=source)
+
+
+@app.route('/twitch')
+def twitchhome():
+    tp = 'Video'
+    source = 'Twitch'
+    return render_template('index.html', tp=tp, source=source)
 
 
 @app.route('/download', methods=['POST'])
-def download():
-
+def get():
     arr = os.listdir(filename)
-
     if len(arr) != 0:
         file = arr[0]
         file_path = filename + file
@@ -32,8 +51,15 @@ def download():
             print("File does not exist")
 
     url = request.form['query']
+    typeFromHtml = request.form['tp']
+    if typeFromHtml == 'Mp3':
+        type = 'bestaudio/best'
+    elif typeFromHtml == 'Twitch' or typeFromHtml == 'Tiktok' or typeFromHtml == 'Mp4' or typeFromHtml == 'Video':
+        type = 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]'
+    else:
+        return render_template('index.html')
     ydl_opts = {
-        'format': 'bestaudio/best',
+        'format': type,
         'outtmpl': 'app/downloads/%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -46,10 +72,39 @@ def download():
             ydl.download([url])
         except DownloadError:
             arr = os.listdir(filename)
-            print(filename)
-            print(len(arr))
             file = arr[0]
             print("Exception has been caught.")
             return send_file(filename + file, as_attachment=True)
     return render_template('index.html')
 
+
+@app.route('/mp4')
+def mp4():
+    tp = 'Mp4'
+    source = 'YouTube'
+    get()
+    return render_template('index.html', tp=tp, source=source)
+
+
+@app.route('/mp3')
+def mp3():
+    tp = 'Mp3'
+    source = 'YouTube'
+    get()
+    return render_template('index.html', tp=tp, source=source)
+
+
+@app.route('/tiktok')
+def tiktok():
+    source = 'TikTok'
+    tp = 'Video'
+    get()
+    return render_template('index.html', tp=tp, source=source)
+
+
+@app.route('/twitch')
+def twitch():
+    source = 'Twitch'
+    tp = 'Video'
+    get()
+    return render_template('index.html', tp=tp, source=source)
